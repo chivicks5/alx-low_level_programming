7aebea841182b227eec58e756f0cefda68d35465
chivicks5 <njokuvictor509@gmail.com>
2023-03-22 14:00:53 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print alphabet - print lower case alphabet only
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	-putchar('\n');
+}
