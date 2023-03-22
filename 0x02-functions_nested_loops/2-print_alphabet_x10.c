2f6cea554b639c20331349999622420f71a4b092
chivicks5 <njokuvictor509@gmail.com>
2023-03-22 14:58:51 -0700

    task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	char b;
+	int count = 0;
+	char letter;
 
-	while (i++ <= 9)
+	while (count++ <= 9)
 	{
-	for (b = 'a'; b <= 'z'; b++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(b);
 	_putchar('/n');
 	}
