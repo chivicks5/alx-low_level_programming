d1b7eb83b77577a1e1c866342ebbb7b3c8f657b6
chivicks5 <njokuvictor509@gmail.com>
2023-03-24 07:49:42 -0700

    task-3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,13 @@
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	char i
+	char c
 
-		for (i >= 0; i <= 9; i++)
+		for (c >= 0; c <= 9; c++)
 		{
-			_putchar(i);
+			_putchar(c);
 		}
 	_putchar("\n");
 }
