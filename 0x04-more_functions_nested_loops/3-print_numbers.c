e88955c6ee8baf03e320c4f39da292c156292609
chivicks5 <njokuvictor509@gmail.com>
2023-03-24 08:18:39 -0700

    task-3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_most_numbers - print numbers from 0-9
+ * print_numbers - print numbers from 0-9
  * Return: void
  */
 
-void print_most_numbers(void)
+void print_numbers(void);
 {
-	char c
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
