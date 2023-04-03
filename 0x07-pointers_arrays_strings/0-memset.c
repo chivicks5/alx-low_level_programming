9a1174e30c92f4e42f5d22d9a6bf96d48ae8eb6d
chivicks5 <njokuvictor509@gmail.com>
2023-04-03 16:33:16 -0700

    task-0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,13 @@
  * Return:s
  */
 
-char *_memset(char *s, char b, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
-        s[i] = b;
-    }
-    return s;
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+
+	{
+		s[i] = b;
+	}
+	return (s);
 }
 
