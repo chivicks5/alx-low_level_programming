01aa764cae78762b99fdaa9cbf013ce1e4131319
chivicks5 <njokuvictor509@gmail.com>
2023-04-05 21:15:22 -0700

    task-5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check - checks for the square root
+ * @n: int to be checked
+ * @a:int
+ * @b:int
+ * Return: -1 if @n not a natural sqrt
+ * chivicks doing hard things
+ */
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}
