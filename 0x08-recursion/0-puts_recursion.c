c6c2ea653ab23f48a5fe57f0cdc04a09d5a3459d
chivicks5 <njokuvictor509@gmail.com>
2023-04-05 19:07:25 -0700

    task-0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,12 +13,12 @@ void _puts_recursion(char *s);
 	if (*s)
 
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 
 }
