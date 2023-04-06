2c33ff8d21c7e462422d83f69d06c8338df9c8e2
chivicks5 <njokuvictor509@gmail.com>
2023-04-06 10:12:10 -0700

    task-6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,23 +7,24 @@
  * chivicks coding
  */
 
-int is_prime_number(int n)
-
+int check(int a, int b)
 {
-	if (n <= 1)
-
-	{
+	if (b < 2 || b % a == 0)
 		return (0);
-	}
-	for (int i = 2; i * i <= n; i++)
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b));
+}
 
-	{
-	if (n % i == 0)
-
-
-	{
-		return (0);
-	}
-	}
-	return (1);
+/**
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check(2, n));
 }
