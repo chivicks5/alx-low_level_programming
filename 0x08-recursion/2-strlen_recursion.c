a2951507d0e46b456bfa1e3f6b5846c759636e4b
chivicks5 <njokuvictor509@gmail.com>
2023-04-05 20:00:07 -0700

    task-2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - this funtion returns the length of a string
+ * @s: the pointing string
+ * Return:lenght of the string
+ * chivicks is comming up
+ */
+
+int _strlen_recursion(char *s)
+
+{
+	int i = 0;
+		if (*s)
+		{
+			i++;
+			s++;
+		}
+		return (i);
+}
