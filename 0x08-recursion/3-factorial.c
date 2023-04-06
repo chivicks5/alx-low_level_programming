bb962422743e47703c1537ccf182cb657d26f82a
chivicks5 <njokuvictor509@gmail.com>
2023-04-05 20:26:50 -0700

    task-3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - the function that returns the factorial of a given number
+ * @n: the given number
+ * Return: n
+ * chivicks can do it better
+ */
+
+int factorial(int n)
+
+{
+	int y;
+
+	if  (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		y = n * (n - 1);
+	}
+	return (y);
+}
