16d55680e1953626683ac7b92fcacb98604089c1
chivicks5 <njokuvictor509@gmail.com>
2023-04-20 19:20:46 -0700

    task-3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: a+b
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: a-b
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: a*b
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: a/b
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: a%b
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
