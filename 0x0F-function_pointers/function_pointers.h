800ed4f93d594c1cf2a5095b490f127dadc40a56
chivicks5 <njokuvictor509@gmail.com>
2023-04-20 18:51:15 -0700

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,10 +7,8 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
 /**
- *  NOI MORE MAIN.H
- * FUNCTION_POINTERS_H
+ * FUNCTION_POINTERS_H - the header file
  *
  */
+#endif
